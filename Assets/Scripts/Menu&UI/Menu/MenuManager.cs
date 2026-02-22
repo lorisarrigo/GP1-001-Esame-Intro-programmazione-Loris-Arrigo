ad204503f208c35b418d42c93591c435ee016bf9
Loris Arrigo <loris.arrigo@gmail.com>
2026-02-22 14:10:10 +0100

    Creazione del menu di inizio
    - Creata una scena Chiamata menu:
     - creato un canva con 2 bottoni:
      - Play (che porta alla scena del gioco);
      - Quit (esce dal gioco);
    
    - creato lo script e un Empty chiamati "MenuManager":
     - gestisce i bottoni dei menu.

---
+++ Assets/Scripts/Menu&UI/Menu/MenuManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum Status
+{
+    GamePaused,
+    GameRunning
+}
+public class MenuManager : MonoBehaviour
+{
+    [Header("Status")]
+    public Status status;
+
+
+    private void Start()
+    {
+        status = Status.GamePaused; //at the start of the game set the status to paused
+    }
+
+    private void Update()
+    {
+        //Setting the TimeScales for the game for when is paused and when is running
+        if (status == Status.GamePaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainLevel"); //Change the scene to the main level
+    }
+
+    //exit the Game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
