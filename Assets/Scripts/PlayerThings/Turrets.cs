e7553c7af8184745e636b42811237f0e4188a325
Loris Arrigo <loris.arrigo@gmail.com>
2026-02-22 22:33:35 +0100

    Creazione bulltes e comportamenti annessi
    - Creati i prefab dei Bullet
    - Creto sctipt per gestione delle stats delle torrette, per il bullet spawner/despawner e lo script per despawnare i nemici davanti alla base

---
+++ Assets/Scripts/PlayerThings/Turrets.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Turrets : MonoBehaviour
+{
+    public GameObject Bullets;
+    public float BulletRate;
+    public float BSpeed;
+
+    [SerializeField] int DamagePerHit;
+    [SerializeField] int Prize;
+}
